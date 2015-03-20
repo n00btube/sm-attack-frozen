59b98be5a7c156235e0458ba34aa540563645481
Adam <snescoder@gmail.com>
2015-03-19 21:31:28 -0400

    New approach: unfreeze on collision.
    If Samus collides with a frozen enemy while speedboosting, shinesparking, or
    screw attacking; unfreeze it, then do the usual collision checks.
    
    Unless, as before, it's a Metroid.  They're pretty w1ck3d.

--- attack-frozen.xkas-06.asm
+++ attack-frozen.xkas-06.asm
@@ -4,30 +4,36 @@
 
 lorom
 
-; main hijack point.  do not edit.
-; this overwrites the freeze timer load during the enemy processing loop.
-; it precedes a BNE - if attack_check returns with the Z flag set, then the
-; "regular" unfrozen enemy code will run.  (Namely, platform detection.)
-org $A0A9DC
-	JSR attack_check
+; main hijack point: intercepts where Samus *has* collided with a platform
+; enemy, either because it's a platform, or because it's frozen.
+org $A0AA2C
+	JMP attack_check
 
-; another necessary bit.  do not edit.
-; the enemy damage routine promptly exits if a freeze timer is set, so without
-; this, Samus harmlessly falls through frozen enemies.
-org $A0A119
-	LDA #$0000           ; replace actual freeze timer with "not frozen"
-
-org $A0FFE0
+; extra implementation, can be anywhere in bank $A0 free space
+; 34 ($22) bytes so far
+org $A0FFD0
 attack_check:
-	LDA $0F9E,X          ; get freeze timer
-	BEQ attack_done      ; not frozen: run default code
-	LDA $0F78,X          ; enemy type
-	CMP #$DD7F           ; metroid?
-	BEQ ice_platform     ; use original freeze behavior
-	LDA $0A6E            ; Samus/enemy collision damage type
-	CMP #$0003           ; screw attacking: do metroid check
-attack_done:
-	RTS                  ; return decision
-ice_platform:
-	LDA $0F9E,X          ; platform if frozen
-	RTS
+	PHX
+	LDX $18A6        ; reload enemy index
+
+	LDA $0F9E,X      ; get freeze timer
+	BEQ platform     ; not frozen: run default code
+
+	LDA $0A6E        ; Samus/enemy collision damage type
+	BEQ platform     ; not special: run default code
+	CMP #$0004       ; first unknown value (1 + screw attack)
+	BCS platform     ; >= unknown? better use the default
+
+	; Samus is speedrunning (1), shinesparking (2), or screw attacking (3)
+	; we should try killing the enemy, if it's not dangerous to do so.
+
+	; exclude super-special enemies
+	LDA $0F78,X      ; enemy type
+	CMP #$DD7F       ; metroid?
+	BEQ platform     ; use original freeze behavior
+
+	; frozen, and not excluded for any reason
+	STZ $0F9E,X      ; unfreeze enemy before processing collision
+platform:
+	PLX
+	JMP ($AA2F,X)
