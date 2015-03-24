88c4bec811204c2ecd4297df6fbbd666e4dcbbb6
Adam <snescoder@gmail.com>
2015-03-23 20:39:07 -0400

    Untested version that ignores touches.

--- attack-frozen.xkas-06.asm
+++ attack-frozen.xkas-06.asm
@@ -6,30 +6,30 @@ lorom
 
 ; main hijack point: intercepts where Samus *has* collided with a platform
 ; enemy, either because it's a platform, or because it's frozen.
-org $A0AA2C
-	JMP attack_check
+org $A0AA24
+	JMP attack_check ; overwrite an LDA that overwrites the collision overlap
 
 ; extra implementation, can be anywhere in bank $A0 free space
 org $A0FFD0
 attack_check:
-	CPX #$0006       ; downward collision type? (*2, it's a word-offset)
-	BEQ landing      ; just land on it, kthx
-
-	PHX
-	LDX $18A6        ; reload enemy index
-
-	LDA $0F9E,X      ; get freeze timer
-	BEQ platform     ; not frozen (platform enemy), normal reaction
+	BEQ platform     ; touched, not collided
 
 	LDA $0A6E        ; Samus/enemy collision damage type
 	BEQ platform     ; zero: nothing special
 	CMP #$0004       ; speedrun/shinespark/screw attack?
 	BCS platform     ; no, some unknown value, do not mess with it
 
-	STZ $0F9E,X      ; unfreeze enemy
-	PLX              ; fix the stack
+	LDA $0B02
+	CMP #$0003       ; downward collision type?
+	BEQ landing      ; just land on it, kthx
+
+	LDX $18A6        ; load enemy index
+	LDA $0F9E,X      ; get freeze timer
+	BEQ platform     ; not frozen (platform enemy), normal reaction
+
+	STZ $0F9E,X      ; unfreeze enemy - normal AI/collision applies
 	JMP $AABF        ; platform miss
 platform:
-	PLX
+	LDA $0B02        ; collision direction, what we overwrote to hijack
 landing:
-	JMP ($AA2F,X)
+	JMP $AA27        ; resume after hijack
